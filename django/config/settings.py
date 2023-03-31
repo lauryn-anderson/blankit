78a6b4e2df45f90cea62d1b1d6202a39759f12ec
lauryn-anderson <laurynelysse@gmail.com>
2023-03-30 22:14:12 -0600

    Add digitalocean to allowed hosts

--- django/config/settings.py
+++ django/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = env.str('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DJANGO_DEBUG', default=False)
 
-ALLOWED_HOSTS = ['.digitalocean.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['.ondigitalocean.app', 'localhost', '127.0.0.1']
 
 
 # Application definition
